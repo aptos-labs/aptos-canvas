6dfaf22699acd56f1d008bc79031779c30dc6fd8
Kent White <kent@aptoslabs.com>
2023-10-16 12:39:12 -0700

    Move other wallets to collapsed state

--- frontend2/src/components/ConnectWalletModal/ConnectWalletModal.tsx
+++ frontend2/src/components/ConnectWalletModal/ConnectWalletModal.tsx
@@ -6,10 +6,12 @@ import {
 } from "@aptos-labs/wallet-adapter-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { css } from "styled-system/css";
 import { flex, stack } from "styled-system/patterns";
 
 import { Button } from "../Button";
+import { ChevronIcon } from "../Icons/ChevronIcon";
 import { ModalContent, openModal } from "../Modal";
 
 export function openConnectWalletModal() {
@@ -21,20 +23,56 @@ export function openConnectWalletModal() {
 
 function ConnectWalletModal({ hide }: { hide: () => void }) {
   const { connect, wallets } = useWallet();
+  const [expanded, setExpanded] = useState(false);
+  const icWallet = wallets.filter((wallet) => wallet.name === "IdentityConnect")[0];
+  const otherWallets = wallets.filter((wallet) => wallet.name !== "IdentityConnect");
 
+  const walletConnectButton = (wallet: Wallet) => {
+    const connectWallet = () => {
+      connect(wallet.name);
+      hide();
+    };
+    return <WalletConnectButton key={wallet.name} wallet={wallet} connect={connectWallet} />;
+  }
   return (
     <ModalContent className={stack({ maxW: "md", gap: 24 })}>
       <h1 className={css({ textStyle: "heading.xl", textAlign: "center" })}>Connect Wallet</h1>
       <div className={stack({ gap: 16 })}>
-        {wallets.map((wallet) => {
-          const connectWallet = () => {
-            connect(wallet.name);
-            hide();
-          };
-          return <WalletConnectButton key={wallet.name} wallet={wallet} connect={connectWallet} />;
-        })}
+        {icWallet ? <WalletConnectButton wallet={icWallet} connect={() => { connect(icWallet.name); }} /> : null}
+        
+        {expanded ? otherWallets.map((wallet) => {
+          return walletConnectButton(wallet)
+        }) : null}
+        <ExpandButton expanded={expanded} setExpanded={setExpanded}/>
       </div>
     </ModalContent>
+  );
+}
+
+interface ExpandButtonProps {
+  expanded: boolean;
+  setExpanded: (expanded: boolean) => void;
+}
+
+function ExpandButton({ expanded, setExpanded }: ExpandButtonProps) {
+  return (
+    <button
+      className={flex({
+        align: "center",
+        justify: "space-between",
+        px: 16,
+        py: 12,
+        gap: 16,
+        bg: "interactive.secondary.hovered",
+        rounded: "lg",
+      })}
+      onClick={() => { setExpanded(!expanded); }}
+    >
+      <div className={css({ textStyle: "body.md.medium", marginY: 4 })}>More Options</div>
+      <div className={css({ paddingX: 16 })}>
+        <ChevronIcon direction={expanded ? 'up' : 'down'} />
+      </div>
+    </button>
   );
 }
 
