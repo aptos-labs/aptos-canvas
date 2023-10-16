5829a2c6c72095f11d9ca6aed968da2fe5728cbf
CapCap <max@aptoslabs.com>
2023-10-16 10:12:14 -0700

    test out other deployed canvas

--- frontend2/src/constants/config.ts
+++ frontend2/src/constants/config.ts
@@ -21,7 +21,7 @@ export const APP_CONFIG: Record<SupportedNetworkName, AppConfig> = {
     canvasAddr: "0x6b8169be66d9579ba9ad1192708edcf52de713d3513a431df6cb045f425d3d91",
     canvasTokenAddr: "0x8c654f4be9cefc3a7d0dfa0bda4ee19f75c926763e00f6534f3ab8b5c2ebcdea",
     canvasImageUrl:
-      "https://storage.googleapis.com/images.testnet.graffio.art/images/0xf1b675e890459dfe0a676c01bd14caca20e35634babccc310b49a14c883ea435.png",
+      "https://storage.googleapis.com/images.testnet.graffio.art/images/0x8c654f4be9cefc3a7d0dfa0bda4ee19f75c926763e00f6534f3ab8b5c2ebcdea.png",
     rpcUrl: "https://fullnode.testnet.aptoslabs.com/",
   },
 };
