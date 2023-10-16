c4f0a84d6ba172dcf8d0810ff4b4dcccc29430ba
Brian Li <brnli7@gmail.com>
2023-10-16 15:31:10 -0400

    Add fetch for draw_enabled_for_non_admin (#94)
    * Add fetch for draw_enabled_for_non_admin
    
    * Delete default_color key
    
    * Default enabled to `true`

--- frontend2/src/components/Canvas/index.tsx
+++ frontend2/src/components/Canvas/index.tsx
@@ -8,6 +8,7 @@ import { DRAW_MODE_ZOOM, PIXELS_PER_SIDE, VIEW_MODE_ZOOM } from "@/constants/can
 import {
   useCanvasCommandListener,
   useCanvasState,
+  useLatestDrawEnabledForNonAdmin,
   useOptimisticUpdateGarbageCollector,
 } from "@/contexts/canvas";
 import { assertUnreachable } from "@/utils/assertUnreachable";
@@ -125,6 +126,7 @@ export function Canvas({ height, width, baseImage, isCursorInBounds }: CanvasPro
   );
 
   useOptimisticUpdateGarbageCollector();
+  useLatestDrawEnabledForNonAdmin();
 
   useEffect(
     function manageViewAndDrawModes() {
