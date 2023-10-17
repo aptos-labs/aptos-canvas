555404abb306573696be468bd913a6685a985b78
Kent White <kent@aptoslabs.com>
2023-10-16 17:13:59 -0700

    Move other wallets to collapsed state (#99)

--- frontend2/src/components/ConnectWalletModal/ConnectWalletModal.tsx
+++ frontend2/src/components/ConnectWalletModal/ConnectWalletModal.tsx
@@ -6,9 +6,11 @@ import {
 } from "@aptos-labs/wallet-adapter-react";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { css } from "styled-system/css";
 import { flex, stack } from "styled-system/patterns";
 
+import { Accordion } from "../Accordion";
 import { Button } from "../Button";
 import { ModalContent, openModal } from "../Modal";
 
@@ -20,21 +22,59 @@ export function openConnectWalletModal() {
 }
 
 function ConnectWalletModal({ hide }: { hide: () => void }) {
+  const [isExpanded, setIsExpanded] = useState(false);
   const { connect, wallets } = useWallet();
+
+  const { defaultWallets, moreWallets } = partitionWallets(wallets);
+
+  const getConnect = (wallet: Wallet) => () => {
+    connect(wallet.name);
+    hide();
+  };
 
   return (
     <ModalContent className={stack({ maxW: "md", gap: 24 })}>
       <h1 className={css({ textStyle: "heading.xl", textAlign: "center" })}>Connect Wallet</h1>
       <div className={stack({ gap: 16 })}>
-        {wallets.map((wallet) => {
-          const connectWallet = () => {
-            connect(wallet.name);
-            hide();
-          };
-          return <WalletConnectButton key={wallet.name} wallet={wallet} connect={connectWallet} />;
+        {defaultWallets.map((wallet) => {
+          return (
+            <WalletConnectButton key={wallet.name} wallet={wallet} connect={getConnect(wallet)} />
+          );
         })}
+        <Accordion trigger="More Options" isExpanded={isExpanded} setIsExpanded={setIsExpanded}>
+          <div className={stack({ gap: 16 })}>
+            {moreWallets.map((wallet) => {
+              return (
+                <WalletConnectButton
+                  key={wallet.name}
+                  wallet={wallet}
+                  connect={getConnect(wallet)}
+                />
+              );
+            })}
+          </div>
+        </Accordion>
       </div>
     </ModalContent>
+  );
+}
+
+function partitionWallets(wallets: ReadonlyArray<Wallet>) {
+  const defaultWallets: Array<Wallet> = [];
+  const moreWallets: Array<Wallet> = [];
+
+  for (const wallet of wallets) {
+    if (isDefaultWallet(wallet)) defaultWallets.push(wallet);
+    else moreWallets.push(wallet);
+  }
+
+  return { defaultWallets, moreWallets };
+}
+
+function isDefaultWallet(wallet: Wallet) {
+  return (
+    wallet.readyState === WalletReadyState.Installed ||
+    wallet.readyState === WalletReadyState.Loadable
   );
 }
 
@@ -50,47 +90,28 @@ function WalletConnectButton({ wallet, connect }: WalletConnectButtonProps) {
   const mobileSupport = wallet.deeplinkProvider;
 
   if (!isWalletReady && isRedirectable()) {
-    if (mobileSupport) {
-      return (
-        <div
-          className={flex({
-            align: "center",
-            justify: "space-between",
-            px: 16,
-            py: 12,
-            gap: 16,
-            bg: "interactive.secondary.hovered",
-            rounded: "lg",
-          })}
-        >
-          <div className={flex({ align: "center", gap: 16 })}>
-            <Image src={wallet.icon} alt={`${wallet.name} icon`} height={24} width={24} />
-            <div className={css({ textStyle: "body.md.medium" })}>{wallet.name}</div>
-          </div>
-          <Button size="xs" onClick={connect}>
-            Connect
-          </Button>
+    if (!mobileSupport) return null;
+    return (
+      <div
+        className={flex({
+          align: "center",
+          justify: "space-between",
+          px: 16,
+          py: 12,
+          gap: 16,
+          bg: "interactive.secondary.hovered",
+          rounded: "lg",
+        })}
+      >
+        <div className={flex({ align: "center", gap: 16 })}>
+          <Image src={wallet.icon} alt={`${wallet.name} icon`} height={24} width={24} />
+          <div className={css({ textStyle: "body.md.medium" })}>{wallet.name}</div>
         </div>
-      );
-    } else {
-      return (
-        <div
-          className={flex({
-            align: "center",
-            justify: "space-between",
-            px: 16,
-            py: 12,
-            gap: 16,
-            rounded: "lg",
-          })}
-        >
-          <div className={flex({ align: "center", gap: 16 })}>
-            <Image src={wallet.icon} alt={`${wallet.name} icon`} height={24} width={24} />
-            <div className={css({ textStyle: "body.md.medium" })}>{wallet.name}</div>
-          </div>
-        </div>
-      );
-    }
+        <Button size="xs" onClick={connect}>
+          Connect
+        </Button>
+      </div>
+    );
   } else {
     const isLoadable =
       wallet.readyState === WalletReadyState.Installed ||
