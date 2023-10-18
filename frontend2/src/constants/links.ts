d9fe96f0d3b7d092da147ef16a6f68555ca1d779
Blake Zimmerman <blake.zimmerman@icloud.com>
2023-10-18 13:55:24 -0400

    New discord link (#117)

--- frontend2/src/constants/links.ts
+++ frontend2/src/constants/links.ts
@@ -1,1 +1,2 @@
-export const DISCORD_CHANNEL = "https://discord.com/channels/945856774056083548/956579023100706837";
+export const DISCORD_CHANNEL =
+  "https://discord.com/channels/945856774056083548/1164251013205721138";
